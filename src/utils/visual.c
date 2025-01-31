caf7f8e9aae0b3637639266ae2c2c755dc0dd1c3
罗杰 <rmarrero@car8s5.42barcelona.com>
2025-01-31 17:53:35 +0100

    visual.c [OK], norminette [OK]

--- src/utils/visual.c
+++ src/utils/visual.c
@@ -1,27 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   visual.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rmarrero <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/31 17:37:43 by rmarrero          #+#    #+#             */
+/*   Updated: 2025/01/31 17:40:05 by rmarrero         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 #include "../../include/push_swap.h"
 
-// Función para visualizar la pila con colores
-void visual(t_stack *stack, int argc)
+// --- Función para visualizar la pila con colores --- //
+void	visual(t_stack *stack, int argc)
 {
-    int i;
-    int tmp;
-    int bg_color;
-    i = argc;
+	int	i;
+	int	tmp;
+	int	bg_color;
 
-    while (stack && i)
-    {
-        tmp = 0;
-        bg_color = 41 + (stack->group % 7);
-        printf("\033[%dm", bg_color);
-        while (tmp < stack->value)
-        {
-            printf(" ");
-            tmp++;
-        }
-        printf("\033[0m\n");
-        stack = stack->next;
-        i--;
-    }
+	i = argc;
+	while (stack && i)
+	{
+		tmp = 0;
+		bg_color = 41 + (stack->group % 7);
+		printf("\033[%dm", bg_color);
+		while (tmp < stack->value)
+		{
+			printf(" ");
+			tmp++;
+		}
+		printf("\033[0m\n");
+		stack = stack->next;
+		i--;
+	}
 }
-//mas detalles dentro de las pilas 
-//printf("Nodo: valor=%d, grupo=%d, color=%d\n", stack->value, stack->group, bg_color);
