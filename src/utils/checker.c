29973587f64d9e41d3f5fd55db6ffa298b1eeffa
罗杰 <rmarrero@car8s5.42barcelona.com>
2025-01-31 17:52:28 +0100

    checker.c [OK], norminette [OK]

--- src/utils/checker.c
+++ src/utils/checker.c
@@ -1,10 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   checker.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rmarrero <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/31 15:19:28 by rmarrero          #+#    #+#             */
+/*   Updated: 2025/01/31 17:52:10 by rmarrero         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 #include "../../include/push_swap.h"
 
-// --- caso de 3 elementos ---
-void checker(t_stack *stack)
+// --- checker --- //
+void	checker(t_stack *stack)
 {
-    if (!sorted(stack))
-        printf("\033[0;31m[KO]\033[0m");
-    else
-        printf("\033[0;32m[OK]\033[0m\n");
+	if (!sorted(stack))
+		ft_printf("\033[0;31m[KO]\033[0m");
+	else
+		ft_printf("\033[0;32m[OK]\033[0m\n");
+}
+
+// --- get stack size --- //
+size_t	size_stack(t_stack *stack)
+{
+	t_stack	*tmp;
+	size_t	i;
+
+	if (!stack)
+		return (0);
+	i = 0;
+	tmp = stack;
+	while (tmp->next && tmp->next != stack)
+	{
+		i++;
+		tmp = tmp->next;
+	}
+	return (i + 1);
 }
