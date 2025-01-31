88764aff33245dc621dd3b6c6501a9562c7077b4
罗杰 <rmarrero@car8s5.42barcelona.com>
2025-01-31 15:14:22 +0100

    push_swap.c [OK], norminette [OK]

--- src/push_swap.c
+++ src/push_swap.c
@@ -1,39 +1,50 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rmarrero <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/31 14:48:41 by rmarrero          #+#    #+#             */
+/*   Updated: 2025/01/31 15:02:00 by rmarrero         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 #include "../include/push_swap.h"
 
-t_stack  *create_a_new_list(char **str_of_values, int argc)
+t_stack	*create_a_new_list(char **str_of_values, int argc)
 {
-    t_stack *tmp;
-    int i;
+	t_stack	*tmp;
+	int		i;
 
-    i = 1;
-    tmp = NULL;
-    while (str_of_values[i])
-    {
-        ft_add_back_the_list(&tmp, ft_create_value(ft_atoi(str_of_values[i])));
-        i++;
-    }
-    ft_linke_first_to_last(&tmp);
-    return (tmp);
+	i = 1;
+	tmp = NULL;
+	while (str_of_values[i])
+	{
+		ft_add_back_the_list(&tmp, ft_create_value(ft_atoi(str_of_values[i])));
+		i++;
+	}
+	ft_linke_first_to_last(&tmp);
+	return (tmp);
 }
 
-// --- elementos ordenados o no ---
-int sorted(t_stack *stack)
+// --- elementos ordenados o no --- //
+int	sorted(t_stack *stack)
 {
-    t_stack *tmp;
-    t_stack *tmp_next;
+	t_stack	*tmp;
+	t_stack	*tmp_next;
 
-    if (!stack || !stack->next)
-        return (1);
-    tmp = stack;
-    tmp_next = stack->next;
-    while (tmp->next != stack)
-    {
-        if (tmp->value > tmp_next->value)
-            return (0);
-        tmp = tmp_next;
-        tmp_next = tmp_next->next;
-    }
-    return (1);
+	if (!stack || !stack->next)
+		return (1);
+	tmp = stack;
+	tmp_next = stack->next;
+	while (tmp->next != stack)
+	{
+		if (tmp->value > tmp_next->value)
+			return (0);
+		tmp = tmp_next;
+		tmp_next = tmp_next->next;
+	}
+	return (1);
 }
 
 // --- filtrar por caso ---
@@ -46,42 +57,41 @@ void	sort_case(t_stack **stack_a, t_stack **stack_b, int argc)
 		sort_three(stack_a);
 	else if (size_stack(*stack_a) == 4)
 		sort_four(stack_a, stack_b);
-    else if (size_stack(*stack_a) == 5)
+	else if (size_stack(*stack_a) == 5)
 		sort_five(stack_a, stack_b);
-    else if (size_stack(*stack_a) == 6)
+	else if (size_stack(*stack_a) == 6)
 		sort_six(stack_a, stack_b);
 	else
-    {
-        assign_chunks(*stack_a, chuncks_opimi(argc));
-        pass_group_to_b(stack_a, stack_b, argc);
+	{
+		assign_chunks(*stack_a, chuncks_opimi(argc));
+		pass_group_to_b(stack_a, stack_b, argc);
 		go_to_a(stack_a, stack_b, argc);
-        if (size_stack(*stack_b) == 1)
-            ft_pa_push_a(stack_a, stack_b);
-        else
-            sort_fixed(stack_a, stack_b);
-    }
+		if (size_stack(*stack_b) == 1)
+			ft_pa_push_a(stack_a, stack_b);
+		else
+			sort_fixed(stack_a, stack_b);
+	}
 }
 
 // --- main --- //
-int main(int argc, char **argv)
+int	main(int argc, char **argv)
 {
-    t_stack *stack_a;
-    t_stack *stack_b;
+	t_stack	*stack_a;
+	t_stack	*stack_b;
 
-    stack_a = NULL;
-    stack_b = NULL;
-    if (!ft_thera_are_error(argc, argv))
-    {
-        free_stack(&stack_a);
-        free_stack(&stack_a);
-        ft_print_errors();
-        return 0;
-    }
-    stack_a = create_a_new_list(argv, argc - 1);
-    if (!sorted(stack_a))
-        sort_case(&stack_a, &stack_b, argc);
-    //ft_printf("%i\n", chuncks_opimi(argc));
-    return (0);
+	stack_a = NULL;
+	stack_b = NULL;
+	if (!ft_thera_are_error(argc, argv))
+	{
+		free_stack(&stack_a);
+		free_stack(&stack_a);
+		ft_print_errors();
+		return (0);
+	}
+	stack_a = create_a_new_list(argv, argc - 1);
+	if (!sorted(stack_a))
+		sort_case(&stack_a, &stack_b, argc);
+	return (0);
 }
 /* cmd > 
 
