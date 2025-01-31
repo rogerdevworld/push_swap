47ab2bb1741a3da090c044a36d966e44722eb92e
罗杰 <rmarrero@car8s5.42barcelona.com>
2025-01-31 13:41:29 +0100

    sort_three.c [OK], norminette [OK]

--- src/sort/sort_three.c
+++ src/sort/sort_three.c
@@ -1,19 +1,17 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   sort_three.c                                    :+:      :+:    :+:   */
+/*   sort_three.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: rmarrero <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/11 23:19:33 by rmarrero          #+#    #+#             */
-/*   Updated: 2025/01/11 23:19:38 by rmarrero         ###   ########.fr       */
+/*   Updated: 2025/01/31 13:41:04 by rmarrero         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
 #include "../../include/push_swap.h"
 
-
-// --- caso de 3 elementos ---
+// --- caso de 3 elementos --- //
 void	sort_three(t_stack **stack)
 {
 	if (min_value(*stack) == (*stack)->value)
