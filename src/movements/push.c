effe07a8692386cc612e2474a3f8028339d78970
罗杰 <rmarrero@car8s5.42barcelona.com>
2025-01-31 15:50:08 +0100

    push.c [OK], norminette [OK]

--- src/movements/push.c
+++ src/movements/push.c
@@ -11,38 +11,6 @@
 /* ************************************************************************** */
 #include "../../include/push_swap.h"
 
-/*
-void	push(t_stack **src, t_stack **dst)
-{
-	t_stack		*tmp;
-
-	if (*src == NULL)
-		return ;
-	tmp = (*src)->next;
-	if (*src == tmp)
-		tmp = NULL;
-	else
-	{
-		(*src)->prev->next = (*src)->next;
-		(*src)->next->prev = (*src)->prev;
-	}
-	if (*dst == NULL)
-	{
-		(*src)->next = *src;
-		(*src)->prev = *src;
-		*dst = *src;
-	}
-	else
-	{
-		(*src)->next = *dst;
-		(*src)->prev = (*dst)->prev;
-		(*dst)->prev->next = *src;
-		(*dst)->prev = *src;
-		*dst = *src;
-	}
-	*src = tmp;
-}
-*/
 void	update_src(t_stack **src)
 {
 	t_stack	*tmp;
@@ -81,16 +49,16 @@ void	push(t_stack **src, t_stack **dst)
 	*dst = tmp;
 }
 
-// pa push a: Takes the first element from stack b and puts it 
-//first in stack a. It does nothing if b is empty.
+// --- pa push a: Takes the first element from stack b and puts it  --- //
+// --- first in stack a. It does nothing if b is empty. --- //
 void	ft_pa_push_a(t_stack **stack_a, t_stack **stack_b)
 {
 	push(stack_b, stack_a);
 	ft_printf("pa\n");
 }
 
-// pb push b: Takes the first element from stack a and puts it 
-//first in stack b. Does nothing if a is empty.
+// --- pb push b: Takes the first element from stack a and puts it  --- //
+// --- first in stack b. Does nothing if a is empty. --- //
 void	ft_pb_push_b(t_stack **stack_a, t_stack **stack_b)
 {
 	push(stack_a, stack_b);
