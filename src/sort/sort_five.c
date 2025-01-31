335448af21779b6d8f1f32c29d08bbe5c76394d5
罗杰 <rmarrero@car8s5.42barcelona.com>
2025-01-31 13:45:05 +0100

    sort_five.c [OK], norminette [OK]

--- src/sort/sort_five.c
+++ src/sort/sort_five.c
@@ -1,24 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   sort_five.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rmarrero <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/31 13:43:13 by rmarrero          #+#    #+#             */
+/*   Updated: 2025/01/31 13:44:49 by rmarrero         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 #include "../../include/push_swap.h"
 
 // --- caso de 5 elementos ---
-void sort_five(t_stack **stack_a, t_stack **stack_b)
+void	sort_five(t_stack **stack_a, t_stack **stack_b)
 {
-    while (size_stack(*stack_a) > 3)
-    {
-        if ((*stack_a)->index <= 2)
-            ft_pb_push_b(stack_a, stack_b);
-        else
-        {
-            if (min_nodo(*stack_a)->index <= size_stack(*stack_a) / 2)
-                ft_ra_rotate_a(stack_a, 1);
-            else
-                ft_rra_reverse_a(stack_a, 1);
-        }
-    }
+	while (size_stack(*stack_a) > 3)
+	{
+		if ((*stack_a)->index <= 2)
+			ft_pb_push_b(stack_a, stack_b);
+		else
+		{
+			if (min_nodo(*stack_a)->index <= size_stack(*stack_a) / 2)
+				ft_ra_rotate_a(stack_a, 1);
+			else
+				ft_rra_reverse_a(stack_a, 1);
+		}
+	}
 	if (!sorted(*stack_a))
 		sort_three(stack_a);
-    ft_pa_push_a(stack_a, stack_b);
-    ft_pa_push_a(stack_a, stack_b);
+	ft_pa_push_a(stack_a, stack_b);
+	ft_pa_push_a(stack_a, stack_b);
 	if (!sorted(*stack_a))
-        ft_sa_swap_a(stack_a, 1);
+		ft_sa_swap_a(stack_a, 1);
 }
